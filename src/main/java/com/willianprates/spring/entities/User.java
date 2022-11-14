2c7448aac76ed593e0768fdfc5a9f007b9da0cf4
WillianPrates <willian.ps97@gmail.com>
2022-11-14 16:30:05 -0300

    H2, JPA repository & popular dados, testado ok

--- src/main/java/com/willianprates/spring/entities/User.java
+++ src/main/java/com/willianprates/spring/entities/User.java
@@ -3,10 +3,23 @@ package com.willianprates.spring.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable{
 	
+
 	private static final long serialVersionUID = 1L;
-	public long id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Long id;
 	private String name;
 	private String email;
 	private String phone;
@@ -16,8 +29,7 @@ public class User implements Serializable{
 		
 	}
 
-	public User(long id, String name, String email, String phone, String passowrd) {
-		super();
+	public User(Long id, String name, String email, String phone, String passowrd) {
 		this.id = id;
 		this.name = name;
 		this.email = email;
@@ -25,11 +37,11 @@ public class User implements Serializable{
 		this.passowrd = passowrd;
 	}
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
